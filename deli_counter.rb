fbeb6a686d8a8bb44841e47a8f27ecd39dfb89ec
Tom Sharkey <tbsharkey@gmail.com>
2021-02-21 19:38:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,4 @@
 
-katz_deli = []
-
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
@@ -13,16 +11,16 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
+def now_serving(deli)
+  if deli.empty?
       puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
+    puts "Currently serving #{deli[0]}."
     katz_deli.shift
   end
 end
